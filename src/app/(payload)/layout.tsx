ee78bbdbe6d39fb0d5beda74321832bd45d9490e
ClausCode <nclaus.code@gmail.com>
2025-02-03 23:52:19 +0300

    Refactor layout files and update metadata for Payload CMS project

--- src/app/(payload)/layout.tsx
+++ src/app/(payload)/layout.tsx
@@ -1,40 +1,43 @@
 /* THIS FILE WAS GENERATED AUTOMATICALLY BY PAYLOAD. */
 /* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME. */
 
-"us";
+"us"
 
-import config from "@payload-config";
-import "@payloadcms/next/css";
-import { RootLayout, handleServerFunctions } from "@payloadcms/next/layouts";
-import type { ServerFunctionClient } from "payload";
-import React, { Suspense } from "react";
+import config from "@payload-config"
+import "@payloadcms/next/css"
+import { RootLayout, handleServerFunctions } from "@payloadcms/next/layouts"
+import type { ServerFunctionClient } from "payload"
+import React, { Suspense } from "react"
 
-import { importMap } from "./admin/importMap.js";
-import "./custom.scss";
+import { importMap } from "./admin/importMap.js"
+import "./custom.scss"
+
+/* THIS FILE WAS GENERATED AUTOMATICALLY BY PAYLOAD. */
+/* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME. */
 
 type Args = {
-  children: React.ReactNode;
-};
+	children: React.ReactNode
+}
 
 const serverFunction: ServerFunctionClient = async function (args) {
-  "use server";
-  return handleServerFunctions({
-    ...args,
-    config,
-    importMap,
-  });
-};
+	"use server"
+	return handleServerFunctions({
+		...args,
+		config,
+		importMap
+	})
+}
 
 const Layout = ({ children }: Args) => (
-  <Suspense>
-    <RootLayout
-      config={config}
-      importMap={importMap}
-      serverFunction={serverFunction}
-    >
-      {children}
-    </RootLayout>
-  </Suspense>
-);
+	<Suspense>
+		<RootLayout
+			config={config}
+			importMap={importMap}
+			serverFunction={serverFunction}
+		>
+			{children}
+		</RootLayout>
+	</Suspense>
+)
 
-export default Layout;
+export default Layout
