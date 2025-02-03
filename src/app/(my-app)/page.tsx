45d8be537c74c450a5daf4cb1ed24c47875ebc58
ClausCode <nclaus.code@gmail.com>
2025-02-03 23:47:31 +0300

    Add Shadcn/UI

--- src/app/(my-app)/page.tsx
+++ src/app/(my-app)/page.tsx
@@ -1,5 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div className="justify-self-center">Example Next.js + Payload CMS App</div>
+    <div className="justify-self-center">
+      <Button>Example Next.js + Payload CMS App</Button>
+    </div>
   );
 }
